860c981dcd30f1a976e9043dd5eca55a87ebe8eb
Maha6468 <motherma7633@gmail.com>
2026-03-25 12:30:31 +0600

    last update

---
+++ lib/Provider/provider_class.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CounterProvider extends ChangeNotifier {
+  int _count = 0;
+  List<int> _history = [0];
+
+  int get count => _count;
+  List<int> get history => _history;
+
+  void increment() {
+    _count++;
+    _history.add(_count);
+    notifyListeners(); // UI update
+  }
+
+  void decrement() {
+    _count--;
+    _history.add(_count);
+    notifyListeners(); // UI update
+  }
+
+  void clearAll() {
+    _count = 0;
+    _history.clear(); // সব history delete
+    notifyListeners();
+  }
+}
