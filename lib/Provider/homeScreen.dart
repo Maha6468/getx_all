edb3b0019070ccd7299096fa5c0958668ac48719
Maha6468 <motherma7633@gmail.com>
2026-03-25 17:48:31 +0600

    consumer provider use , just niddristo widget rebuild  hoicce

--- lib/Provider/homeScreen.dart
+++ lib/Provider/homeScreen.dart
@@ -1,3 +1,84 @@
+//
+// import 'package:flutter/material.dart';
+// import 'package:getx_all/Provider/provider_class.dart';
+// import 'package:provider/provider.dart';
+//
+// class HomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     print("Maha");
+//     final provider = context.watch<CounterProvider>();
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Counter App"),
+//         centerTitle: true,
+//         backgroundColor: Colors.cyanAccent,
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//
+//           SizedBox(height: 10),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: () {
+//                   context.read<CounterProvider>().decrement();
+//                   print("MD");
+//                 },
+//                 child: Icon(Icons.remove),
+//               ),
+//               SizedBox(width: 20),
+//               Text(
+//                 provider.count.toString(),
+//                 style: TextStyle(fontSize: 40),
+//               ),
+//               SizedBox(width: 20),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   context.read<CounterProvider>().increment();
+//                   print("Rahman");
+//                 },
+//                 child: Icon(Icons.add),
+//               ),
+//
+//             ],
+//           ),
+//           Expanded(
+//             child: ListView.builder(
+//               itemCount: provider.history.length,
+//               itemBuilder: (context, index) {
+//                 final reversedIndex =
+//                     provider.history.length - 1 - index;
+//                 return Center(
+//                   child: Text(
+//                     "Last Update: ${provider.history[reversedIndex].toString()}",
+//                     style: TextStyle(fontSize: 20),
+//                   ),
+//                 );
+//               },
+//             ),
+//           ),
+//           ElevatedButton(
+//             onPressed: () {
+//               context.read<CounterProvider>().clearAll();
+//             },
+//             style: ElevatedButton.styleFrom(
+//               backgroundColor: Colors.red,
+//             ),
+//             child: Text("Clear"),
+//           ),
+//           SizedBox(height: 30,)
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+/// consumer diye kora ,,uporer ta watch diye kora
 
 import 'package:flutter/material.dart';
 import 'package:getx_all/Provider/provider_class.dart';
@@ -7,44 +88,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("Maha");
-    final provider = context.watch<CounterProvider>();
-
+    //final provider = context.watch<CounterProvider>();
     return Scaffold(
-
       appBar: AppBar(
         title: Text("Counter App"),
         centerTitle: true,
         backgroundColor: Colors.cyanAccent,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start, // 🔥 উপরে রাখবে
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
 
-          SizedBox(height: 10), // 👈 একটু gap (optional)
+          SizedBox(height: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // ➖ Decrement Button
               ElevatedButton(
                 onPressed: () {
                   context.read<CounterProvider>().decrement();
-                  print("majala");
+                  print("MD");
                 },
                 child: Icon(Icons.remove),
               ),
-
               SizedBox(width: 20),
+              Consumer<CounterProvider>(
+                builder: (context, provider, child) {
+                  //print("🔥 Only Text rebuild hocche");
 
-              // 🔢 Counter Text
-              Text(
-                provider.count.toString(),
-                style: TextStyle(fontSize: 40),
+                  return Text(
+                    provider.count.toString(),
+                    style: TextStyle(fontSize: 40),
+                  );
+                },
               ),
-
               SizedBox(width: 20),
-
-              // ➕ Increment Button
               ElevatedButton(
                 onPressed: () {
                   context.read<CounterProvider>().increment();
@@ -53,27 +131,40 @@ class HomeScreen extends StatelessWidget {
                 child: Icon(Icons.add),
               ),
 
-
-
             ],
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: provider.history.length,
-              itemBuilder: (context, index) {
-                final reversedIndex =
-                    provider.history.length - 1 - index;
+          //Expanded(
+            // child:  ListView.builder(
+            //   itemCount: provider.history.length,
+            //   itemBuilder: (context, index) {
+            //     final reversedIndex =
+            //         provider.history.length - 1 - index;
+            //     return Center(
+            //       child: Text(
+            //         "Last Update: ${provider.history[reversedIndex].toString()}",
+            //         style: TextStyle(fontSize: 20),
+            //       ),
+            //     );
+            //   },
+            // ),
+            Expanded(
+              child: Consumer<CounterProvider>(
+                builder: (context, provider, child) {
+                  return ListView.builder(
+                    itemCount: provider.history.length,
+                    itemBuilder: (context, index) {
+                      final reversedIndex =
+                          provider.history.length - 1 - index;
 
-                return Center(
-                  child: Text(
-                    "Last Update: ${provider.history[reversedIndex].toString()}",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                );
-              },
+                      return Text(
+                        "Last Update: ${provider.history[reversedIndex]}",
+                      );
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-
+         // ),
           ElevatedButton(
             onPressed: () {
               context.read<CounterProvider>().clearAll();
@@ -84,7 +175,6 @@ class HomeScreen extends StatelessWidget {
             child: Text("Clear"),
           ),
           SizedBox(height: 30,)
-
         ],
       ),
     );
